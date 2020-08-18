3c954f3efec6ec817017e845420f66a6fccf5b1c
95ktsmith <95ktsmith@gmail.com>
2020-08-18 04:49:26 +0000

    Move all unpacked files up one level.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,11 +12,16 @@ def do_deploy(archive_path):
         try:
             filename = archive_path[archive_path.find('/') + 1:
                                     archive_path.find('.')]
+            ext = archive_path[archive_path.find('.') + 1:]
             put(archive_path, "/tmp/")
             run("sudo mkdir /data/web_static/releases/{}".format(filename))
             run("sudo tar -zxf /tmp/{}.tgz -C /data/web_static/releases/{}".
                 format(filename, filename))
             run("sudo rm /tmp/{}.tgz".format(filename))
+            run("sudo mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}".format(filename, filename))
+            run("sudo rmdir /data/web_static/releases/{}/web_static".
+                format(filename))
             run("sudo rm /data/web_static/current")
             run("sudo ln -s /data/web_static/releases/{}\
                  /data/web_static/current".format(filename))
