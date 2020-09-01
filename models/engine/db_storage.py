7cfbcdc97444620162d1bbd0843184f202a50f81
95ktsmith <95ktsmith@gmail.com>
2020-09-01 13:31:01 +0000

    Updating with close() methods.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -49,7 +49,7 @@ class DBStorage():
             objects = objects + self.__session.query(User).all()
             objects = objects + self.__session.query(Review).all()
         else:
-            objects = self.__sesion.query(cls).all()
+            objects = self.__session.query(cls).all()
 
         # Build dictionary from list. Key format is <Class name>.<object id>
         for obj in objects:
@@ -91,3 +91,7 @@ class DBStorage():
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """ Closes current session """
+        self.__session.close()
